b9cb703be80a96f2959d8a0e4b0cff4cab0f7a7c
Abu Huraira Arain <arainhuraira@gmail.com>
2024-04-01 03:35:56 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,19 @@
+#! usr/bin/env node
+import inquirer from "inquirer"
+
+let randomNumber = Math.floor(Math.random() * 10 + 1);
+
+let answer = await inquirer.prompt([
+    {
+        name: "guessedGame",
+        type: "number",
+        message: "Select between 1 to 10 :",
+    },
+]);
+if (randomNumber === answer.guessedGame) {
+    console.log("Congratulations!!! You guessed right number");
+
+} else {
+    console.log("You guessed wrong number");
+
+}
